0886c07f95ae46b1886897c6d4ac3a0e10906384
ogrobertino <ogrobertino2@gmail.com>
2025-05-27 22:06:09 +0300

    i guess final

--- src/global_params.py
+++ src/global_params.py
@@ -4,5 +4,5 @@ import os
 COL_TREATMENT = "treatment"
 COL_TARGET = "target"
 BASE_PATH = str(pathlib.Path(os.path.dirname(os.path.abspath(__file__))).parent)
-EXPS_PATH = "exps2"
+EXPS_PATH = "exps3"
 SEED = 1237451
